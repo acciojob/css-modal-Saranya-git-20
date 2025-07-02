b03c487c2775772a89287e746ad4f53018916921
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-02 22:15:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-//your JS code here. If required.
+const openBtn = document.getElementById("openModal");
+  const modal = document.querySelector(".modal");
+  const closeBtn = document.querySelector(".close-modal");
+  openBtn.addEventListener("click", () => {
+    modal.style.display = "block";
+  });
+
+  closeBtn.onclick = () => {
+    modal.style.display = "none";
+  }
+
+  window.onclick = (e) => {
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+  }
